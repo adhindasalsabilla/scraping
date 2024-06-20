3052bb3f97d18bb816cf1abe3c52c4b6cdb013d5
adhindasalsabilla <126484597+adhindasalsabilla@users.noreply.github.com>
2024-06-20 18:49:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,17 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Baca data dari file PDF
+# Baca data dari file CSV
 data = pd.read_csv('imdb_primary_uas.csv')
+
+# Menampilkan judul di halaman web
+st.title("Scraping IMDB")
+
+# Membaca file CSV ke dalam DataFrame dengan encoding 'latin1'
+df1 = pd.read_csv(fn1, encoding='latin1')
+
+# Menampilkan DataFrame sebagai tabel
+st.dataframe(df1)
 
 # Filter 5 film teratas dengan durasi terlama
 top_5 = data.sort_values(by='Durasi(Menit)', ascending=False).head(5)
